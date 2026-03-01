05ca3b5b99fe365bcaa0ba15d79903e618e6aebd
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2026-03-01 14:08:04 +0500

    add dto

---
+++ Backend_Amethyst_Audio/DTO/Pages/PageMyRecordPersonalizedDto.cs
@@ -0,0 +1,9 @@
+namespace Backend_Amethyst_Audio.DTO.Pages;
+
+public class PageMyRecordPersonalizedDto
+{
+    public string PaceName { get; set; }
+    public string MoodName { get; set; }
+    public bool IsTextless { get; set; }
+    public string? Country { get; set; }
+}
