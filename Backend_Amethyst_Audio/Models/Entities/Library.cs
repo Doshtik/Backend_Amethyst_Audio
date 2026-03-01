8c7653c457403f047f926ea3133111787fe4d51e
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2026-03-01 14:07:18 +0500

    add library

---
+++ Backend_Amethyst_Audio/Models/Entities/Library.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend_Amethyst_Audio.Models.Entities;
+
+public partial class Library
+{
+    public long Id { get; set; }
+
+    public long IdUser { get; set; }
+
+    public virtual User IdUserNavigation { get; set; } = null!;
+
+    public virtual ICollection<LibrariesTrack> LibrariesTracks { get; set; } = new List<LibrariesTrack>();
+}
